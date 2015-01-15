3da87beb514114d3ad9f8c7bdb456722c11dfc53
Steven Sinatra <ohai@diagramatics.me>
2015-01-15 23:55:08 +1100

    Quick order styling

--- app/views/products/category.php
+++ app/views/products/category.php
@@ -8,14 +8,14 @@
       <img class="product-category-item-image" src="/img/products/<?= $item->image ?>" alt="<?= $item->prodName ?>" />
     </a>
     <div class="product-category-item-details">
-      <a class="product-category-item-link" href="/products/product/<?= $item->prodID . '/' . Helpers::makeSlug($item->prodName) ?>">
+      <a class="product-category-item-name-link" href="/products/product/<?= $item->prodID . '/' . Helpers::makeSlug($item->prodName) ?>">
         <div class="product-category-item-name"><?= $item->prodName ?></div>
       </a>
       <form action="?addBag" class="product-category-item-add" method="POST">
-        <input type="number" name="qty" value="1" min="1" max="<?= $item->quantity ?>">
+        <input class="product-category-item-add-qty" type="number" name="qty" value="1" min="1" max="<?= $item->quantity ?>">
         <input type="hidden" name="itemQty" value="<?= $item->quantity ?>">
         <input type="hidden" name="itemID" value="<?= $item->prodID ?>">
-        <button title="Add to Cart"><svg class="icon-plus"><use xlink:href="#icon-plus"></use></svg></button>
+        <button class="button-icon" title="Add to Bag"><svg class="icon-plus"><use xlink:href="#icon-plus"></use></svg></button>
       </form>
     </div>
   </div>
