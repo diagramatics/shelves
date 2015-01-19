70c736abded2ae0fd5d94d63d6419df3fcd50d26
Steven Sinatra <ohai@diagramatics.me>
2015-01-19 14:05:27 +1100

    Basic navigation on admin panel (no styling)

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,4 +1,12 @@
 <div class="container">
-  <a href="/admin/product/add">Add Product</a>
-  <a href="/admin/category/add">Add Category</a>
+  <a href="/admin/product">Products</a>
+  <ul>
+    <li><a href="/admin/product/add">Add Product</a></li>
+  </ul>
+  <a href="/admin/category">Categories</a>
+  <ul>
+    <li><a href="/admin/category/add">Add Category</a></li>
+    <li><a href="/admin/subcategory">Subcategories</li>
+    <li><a href="/admin/subcategory/add">Add Subcategory</a></li>
+  </ul>
 </div>
