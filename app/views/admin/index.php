7fddec985a45bb60d1dd3d82a76560a482c96c6a
Brendelle <brendelleonte@gmail.com>
2015-01-23 14:23:54 +1100

    Unclosed element a.

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -6,7 +6,7 @@
   <a href="/admin/category">Categories</a>
   <ul>
     <li><a href="/admin/category/add">Add Category</a></li>
-    <li><a href="/admin/subcategory">Subcategories</li>
+    <li><a href="/admin/subcategory">Subcategories</a></li>
     <li><a href="/admin/subcategory/add">Add Subcategory</a></li>
   </ul>
 </div>
