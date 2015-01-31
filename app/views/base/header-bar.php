824b7671ed5f0f60d75d29061d477a23db7e15d3
Steven Sinatra <ohai@diagramatics.me>
2015-01-31 15:30:40 +1100

    Tweak styles on header buttons

--- app/views/base/header-bar.php
+++ app/views/base/header-bar.php
@@ -14,11 +14,10 @@
   <?php if (!isset($_SESSION["email"])): ?>
   <div class="site-meta site-sign-in" aria-hidden="true">
     <a id="metaBackButton" href="#" class="site-sign-in-back"><svg class="icon-arrow-left"><use xlink:href="#icon-arrow-left"></use></svg></a>
-    <form id="" class="site-sign-in-form" action="?login" method="POST">
+    <form id="login" class="site-sign-in-form" action="?login" method="POST">
       <input type="email" name="loginEmail" value="" placeholder="Email">
       <input type="password" name="loginPassword" value="" placeholder="Password">
       <button class="site-meta-buttons">Sign In</button>
-      <a href="#" class="site-meta-buttons site-sign-in-facebook"><svg class="icon-facebook"><use xlink:href="#icon-facebook"></use></svg></a>
       <a href="#" class="site-meta-buttons site-sign-in-register" data-popup="register">Register</a>
     </form>
   </div>
@@ -32,12 +31,12 @@
   </div>
   <?php endif ?>
   <div class="site-account">
-    <span class="site-header-buttons tooltip--bottom" data-tooltip="<?= isset($_SESSION['email']) ? 'Account' : 'Sign In'; ?>">
+    <div class="site-header-buttons tooltip--bottom" data-tooltip="<?= isset($_SESSION['email']) ? 'Account' : 'Sign In'; ?>">
       <a id="metaButton" href="#"><svg class="icon-user"><use xlink:href="#icon-user"></use></svg></a>
-    </span>
-    <span class="site-header-buttons tooltip--bottom" data-tooltip="Bag">
+    </div>
+    <div class="site-header-buttons tooltip--bottom" data-tooltip="Bag">
       <a href="/bag"><svg class="icon-bag"><use xlink:href="#icon-bag"></use></svg></a>
-    </span>
+    </div>
     <div class="site-header-buttons tooltip--bottom" data-tooltip="Menu">
       <a id="menuButton" href="#"><svg class="icon-menu"><use xlink:href="#icon-menu"></use></svg></a>
       <div id="menuButtonPopup" class="menu-button-popup">
