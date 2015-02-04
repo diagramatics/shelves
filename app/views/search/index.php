c1f8ff11bd0c5806f498fdcca5707a569c30dadf
Steven Sinatra <ohai@diagramatics.me>
2015-02-04 16:02:28 +1100

    Search pagination styling and bugfixes

--- app/views/search/index.php
+++ app/views/search/index.php
@@ -2,7 +2,7 @@
   <header>
     <h1 class="product-categories-name">Search Results</h1>
     <p class="specials-desc">There <?= $data['count'] != 1 ? 'are' : 'is' ?> <?= $data['count'] ?> result<?= $data['count'] != 1 ? 's' : '' ?> from your search.</p>
-    <p class="specials-desc">Showing <?= (20 * $data['n']) + 1 ?> – <?= min((20 * (max($data['n'], 1) + 1)), $data['count']) ?></p>
+    <p class="specials-desc">Showing <?= (20 * $data['n']) + 1 ?> – <?= min(((20 * ($data['n']+1))), $data['count']) ?></p>
   </header>
 
   <div class="block-grid">
@@ -16,10 +16,12 @@
     <?php endforeach ?>
   </div>
 
-  <?php if ($data['n'] > 0): ?>
-    <a class="search-result-previous" href="?q=<?= $data['q'] ?>&n=<?= $data['n'] - 1 ?>">Previous</a>
-  <?php endif ?>
-  <?php if ($data['count'] > 20 + (20 * $data['n'])): ?>
-    <a class="search-result-next" href="?q=<?= $data['q'] ?>&n=<?= $data['n'] + 1 ?>">Next</a>
-  <?php endif ?>
+  <div class="search-result-pagination">
+    <?php if ($data['n'] > 0): ?>
+      <a class="search-result-previous" href="?q=<?= $data['q'] ?>&n=<?= $data['n'] - 1 ?>">Previous &laquo;</a>
+    <?php endif ?>
+    <?php if ($data['count'] > 20 + (20 * $data['n'])): ?>
+      <a class="search-result-next" href="?q=<?= $data['q'] ?>&n=<?= $data['n'] + 1 ?>">Next &raquo;</a>
+    <?php endif ?>
+  </div>
 </section>
