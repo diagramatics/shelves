a4fbd350ba1844a1c1668fdcd37e3fb4d1888c8b
Brendelle <brendelleonte@gmail.com>
2015-02-06 01:07:30 +1100

    Style admin page

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,20 +1,36 @@
 <section class="container">
+  <div class="admin-panel-page">
   <h1>Admin Panel</h1>
-  <a href="/admin/product">Products</a>
-  <ul>
-    <li><a href="/admin/product/add">Add Product</a></li>
-  </ul>
-  <a href="/admin/category">Categories</a>
-  <ul>
-    <li><a href="/admin/category/add">Add Category</a></li>
-  </ul>
-  <a href="/admin/subcategory">Subcategories</a>
-  <ul>
-    <li><a href="/admin/subcategory/add">Add Subcategory</a></li>
-  </ul>
-  <a href="/admin/specials">Specials</a>
-  <ul>
-    <li><a href="/admin/specials/add">Add Specials</a></li>
-  </ul>
-  <a href="/admin/users">Users</a>
+    <div class="admin-panel-box1">
+      <a href="/admin/product"><div class="product"></div></a>
+      <ul>
+        <li><a href="/admin/product/add">Product</a></li>
+      </ul>
+    </div>
+    <div class="admin-panel-box2">
+    <a href="/admin/category"><div class="categories"></div></a>
+    <ul>
+      <li><a href="/admin/category/add">Category</a></li>
+    </ul>
+    </div>
+    <div class="admin-panel-box3">
+    <a href="/admin/subcategory"><div class="subcategories"></div></a>
+    <ul>
+      <li><a href="/admin/subcategory/add">Subcategory</a></li>
+    </ul>
+    </div>
+    <div class="admin-panel-box4">
+    <a href="/admin/specials"><div class="specials"></div></a>
+    <ul>
+      <li><a href="/admin/specials/add">Specials</a></li>
+    </ul>
+    </div>
+    <div class="admin-panel-box5">
+    <a href="/admin/users"><div class="users"></div></a>
+    <ul>
+      <li><a href="#">User</a></li>
+    </ul>
+    </div>
+  </div>
+  <div class="clear"></div>
 </section>
