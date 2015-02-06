5e93d581084ba85b611831833ecc589f2e791885
Brendelle <brendelleonte@gmail.com>
2015-02-06 14:17:19 +1100

    new css update

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,36 +1,37 @@
 <section class="container">
   <div class="admin-panel-page">
   <h1>Admin Panel</h1>
-    <div class="admin-panel-box1">
-      <a href="/admin/product"><div class="product"></div></a>
-      <ul>
-        <li><a href="/admin/product/add">Product</a></li>
-      </ul>
-    </div>
-    <div class="admin-panel-box2">
-    <a href="/admin/category"><div class="categories"></div></a>
-    <ul>
-      <li><a href="/admin/category/add">Category</a></li>
-    </ul>
-    </div>
-    <div class="admin-panel-box3">
-    <a href="/admin/subcategory"><div class="subcategories"></div></a>
-    <ul>
-      <li><a href="/admin/subcategory/add">Subcategory</a></li>
-    </ul>
-    </div>
-    <div class="admin-panel-box4">
-    <a href="/admin/specials"><div class="specials"></div></a>
-    <ul>
-      <li><a href="/admin/specials/add">Specials</a></li>
-    </ul>
-    </div>
-    <div class="admin-panel-box5">
-    <a href="/admin/users"><div class="users"></div></a>
-    <ul>
-      <li><a href="#">User</a></li>
-    </ul>
-    </div>
+      <div class="admin-panel-box1">
+        <a href="/admin/product"><div class="product"></div></a>
+      </div>
+      <div class="admin-panel-box2">
+      <a href="/admin/category"><div class="categories"></div></a>
+      </div>
+      <div class="admin-panel-box3">
+      <a href="/admin/subcategory"><div class="subcategories"></div></a>
+      </div>
+      <div class="admin-panel-box4">
+      <a href="/admin/specials"><div class="specials"></div></a>
+      </div>
+      <div class="admin-panel-box5">
+      <a href="/admin/users"><div class="users"></div></a>
+      </div>
+    <div class="clear"></div>
+      <div class="admin-panel-box6">
+          <a href="/admin/product/add">Product</a>
+      </div>
+      <div class="admin-panel-box7">
+        <a href="/admin/category/add">Category</a>
+      </div>
+      <div class="admin-panel-box8">
+        <a href="/admin/subcategory/add">Subcategory</a>
+      </div>
+      <div class="admin-panel-box9">
+        <a href="/admin/specials/add">Specials</a>
+      </div>
+      <div class="admin-panel-box10">
+        <a href="#">User</a>
+      </div>
   </div>
   <div class="clear"></div>
 </section>
